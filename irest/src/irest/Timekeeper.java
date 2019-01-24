b911a3367ae1354b825a87df9e4a2a90c8269459
Navin <navinipe@gmail.com>
2019-01-24 19:34:38 +0530

    Separated check time interval and write time interval. Added a check for sufficient rest.

--- irest/src/irest/Timekeeper.java
+++ irest/src/irest/Timekeeper.java
@@ -9,28 +9,48 @@ import java.time.Instant;
 import sounds.PlaySound;
 
 public class Timekeeper {
-    private final int checkDelay = 5;//minutes
-    private final int seconds = 1000;
-    private final int minutes = 60;
-    private final int pauseTime = checkDelay * seconds * minutes;
-    private long lastBeep;
-    //private final long pauseTime = checkDelay * seconds;//for testing
+    private final int minutesToPause = 1;//program goes to sleep for these many minutes
+    private final int writeToFileInterval = 5;//program writes to file at this interval (in minutes)
+    private final int toMilli = 1000;
+    private final int toSecond = 60;
+    private final int toHours = 60 * this.toSecond;
+    private final int pauseDelay = this.minutesToPause * this.toMilli * this.toSecond;//Unit: milliseconds
+    private final int writeDelay = this.writeToFileInterval * this.toMilli * this.toSecond;//Unit: milliseconds
+    //private final int pauseDelay = 10000;//for testing
+    private final long beepDelay = 1*this.toHours;//Unit: seconds. 1 hour beeps
+    private final long sufficientRestTime = 30*this.toSecond;//Unit: seconds. 30 minute rest time is considered sufficient
+    private long lastBeep;    
+    private long lastWrite;    
     private final DataManager data;
     private final PlaySound sounds;
-    
+    //TODO: move intervals of data and sound to respective classes if necessary
     public Timekeeper() {
         this.lastBeep = 0;
+        this.lastWrite = 0;
         this.data = new DataManager();
         this.sounds = new PlaySound();
     }
     
-    public long GetPauseDelay() {return pauseTime;}
-    public void RecordActivity() {//means that the User is currently using the computer
+    public long GetPauseDelay() {return pauseDelay;}
+    
+    public void RecordAndNotifyUserIfTimeElapsed() {//means that the User is currently using the computer
         long timeNow = Instant.now().getEpochSecond();
-        this.data.RecordActiveTime(timeNow);
-        if (timeNow - this.lastBeep > minutes*minutes) {//play a beep every hour
-            sounds.PlayBeep();
+        
+        if (timeNow - this.lastWrite >= this.writeToFileInterval) {//write to file if interval elapsed
+            this.data.RecordActiveTime(timeNow);
+            this.lastWrite = timeNow;
+        }            
+        
+        if (timeNow - this.lastBeep >= this.beepDelay) {//play sound if an hour elapsed
+            this.sounds.PlayBeep();
             this.lastBeep = timeNow;
         }
+    }
+    
+    public void ResetBeepWarningIfSufficientRestObtained() {
+        long timeNow = Instant.now().getEpochSecond();
+        if (timeNow - this.lastBeep >= this.sufficientRestTime) {
+            this.lastBeep = timeNow;
+        }        
     }
 }
