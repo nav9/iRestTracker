f6dd6269aa395172aae2ab32a3e32cdea6eaf9ab
Navin <navinipe@gmail.com>
2019-01-24 23:55:36 +0530

    Added a 5 minute reminder beep after every hour

--- irest/src/irest/Timekeeper.java
+++ irest/src/irest/Timekeeper.java
@@ -17,14 +17,18 @@ public class Timekeeper {
     private final int pauseDelay = this.minutesToPause * this.toMilli * this.toSecond;//Unit: milliseconds
     private final int writeDelay = this.writeToFileInterval * this.toMilli * this.toSecond;//Unit: milliseconds
     //private final int pauseDelay = 10000;//for testing
-    private final long beepDelay = 1*this.toHours;//Unit: seconds. 1 hour beeps
-    private final long sufficientRestTime = 30*this.toSecond;//Unit: seconds. 30 minute rest time is considered sufficient
+    private final long beepDelay = 1 * this.toHours;//Unit: seconds. 1 hour beeps
+    private final long sufficientRestTime = 30 * this.toSecond;//Unit: seconds. 30 minute rest time is considered sufficient
+    private final long reminderInterval = 5 * this.toSecond;//reminds User to rest
+    private boolean reminderActive;
     private long lastBeep;    
     private long lastWrite;    
     private final DataManager data;
     private final PlaySound sounds;
     //TODO: move intervals of data and sound to respective classes if necessary
+    
     public Timekeeper() {
+        this.reminderActive = false;
         this.lastBeep = 0;
         this.lastWrite = 0;
         this.data = new DataManager();
@@ -43,14 +47,22 @@ public class Timekeeper {
         
         if (timeNow - this.lastBeep >= this.beepDelay) {//play sound if an hour elapsed
             this.sounds.PlayBeep();
+            this.reminderActive = true;
             this.lastBeep = timeNow;
         }
+        
+        if (timeNow - this.lastBeep >= this.reminderInterval && this.reminderActive) {//play sound if an hour elapsed and User is still using computer
+            this.sounds.PlayBeep();
+            this.lastBeep = timeNow;
+            //reminder is deactivated in ResetBeepWarningIfSufficientRestObtained()
+        }        
     }
     
     public void ResetBeepWarningIfSufficientRestObtained() {
         long timeNow = Instant.now().getEpochSecond();
         if (timeNow - this.lastBeep >= this.sufficientRestTime) {
             this.lastBeep = timeNow;
+            this.reminderActive = false;
         }        
     }
 }
